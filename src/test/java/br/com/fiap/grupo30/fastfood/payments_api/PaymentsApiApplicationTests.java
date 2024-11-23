6e75355533b174a8345f7f1ac1d7e511cd67be6d
Otavio Andrade <56943487+otavio-code@users.noreply.github.com>
2024-11-23 02:45:34 -0300

    Update PaymentsApiApplicationTests.java

--- src/test/java/br/com/fiap/grupo30/fastfood/payments_api/PaymentsApiApplicationTests.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/payments_api/PaymentsApiApplicationTests.java
@@ -1,16 +1,15 @@
 package br.com.fiap.grupo30.fastfood.payments_api;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class PaymentsApiApplicationTests {
 
     @Test
     void contextLoads() {
-        // Verifica se o contexto da aplicação carrega corretamente
         assertTrue(true, "O contexto foi carregado com sucesso.");
     }
 }
