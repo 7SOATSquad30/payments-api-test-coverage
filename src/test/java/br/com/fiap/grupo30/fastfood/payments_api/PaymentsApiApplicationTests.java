bde800712262510335300254682e2ece44c7cba2
Otavio Andrade <56943487+otavio-code@users.noreply.github.com>
2024-11-23 23:16:08 -0300

    Update PaymentsApiApplicationTests.java

--- src/test/java/br/com/fiap/grupo30/fastfood/payments_api/PaymentsApiApplicationTests.java
+++ src/test/java/br/com/fiap/grupo30/fastfood/payments_api/PaymentsApiApplicationTests.java
@@ -1,19 +1,20 @@
 package br.com.fiap.grupo30.fastfood.payments_api;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class PaymentsApiApplicationTests {
 
-    @Autowired private ApplicationContext context;
+    @Autowired
+    private ApplicationContext context;
 
     @Test
     void contextLoads() {
-        assertNotNull(context, "O contexto da aplicação não foi carregado corretamente.");
+        assertNotNull(context, "Application context should load.");
     }
 }
